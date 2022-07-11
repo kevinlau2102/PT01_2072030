f26cde7c12a71c0b38715a06c9d57a1d08baf49d
kevinlau2102 <kevinlaurence49@gmail.com>
2022-07-11 12:35:43 +0700

    Upload PT01C

---
+++ PT01C_2072030/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.pt01c_2072030 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.pt01c_2072030 to javafx.fxml;
+    exports com.example.pt01c_2072030;
+}
